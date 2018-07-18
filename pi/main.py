4c3a46a7659a936a31e4f41acf76fe43b24f001d
Calin <csiz@users.noreply.github.com>
2018-07-18 16:24:56 -0400

    Graceful shutdown by reading from stdin.
    Handling KeyboardInterrupt exceptions is unreliable because it might be
    raised in the middle of the event loop, thus leaving it in a weird
    state from which we can't recover.

--- pi/main.py
+++ pi/main.py
@@ -3,6 +3,7 @@ import dataclasses
 import typing
 import logging
 import traceback
+import sys
 
 from . pi_scripts import gyro
 from . websocket_server import ObservableServer
@@ -17,16 +18,27 @@ def main():
 
   print("Gather and run all tasks!")
   async def async_main():
+    # Start all the tasks we need to run.
+    serve_forever = asyncio.ensure_future(server.serve_forever())
+    relay_gyro_0 = asyncio.ensure_future(server.trigger_from(gyro_0))
 
-    # We need to create these tasks within a running event loop.
-    tasks = [
-      server.serve_forever(),
-      server.trigger_from(gyro_0),
-    ]
+    def handle_input():
+      command = sys.stdin.readline().strip()
+      if command == "stop":
+        print("Stopping!")
+        server.stop()
+        relay_gyro_0.cancel()
+      else:
+        print(f"Unknown command: {command}")
 
-    # Run all pending tasks, loggint any exceptions.
-    for task in asyncio.as_completed(tasks):
-      await task
+    # Run all pending tasks.
+    tasks = [relay_gyro_0, serve_forever]
+    try:
+      asyncio.get_event_loop().add_reader(sys.stdin.fileno(), handle_input)
+      await complete_all(tasks)
+    finally:
+      asyncio.get_event_loop().remove_reader(sys.stdin.fileno())
+
 
   try:
     # Sigh, no good way to gracefully exit on KeyboardInterrupt...
@@ -41,6 +53,12 @@ def main():
 def print_exception(exc):
   return traceback.print_exception(type(exc), exc, exc.__traceback__)
 
+async def complete_all(tasks):
+  for task in asyncio.as_completed(tasks):
+    try:
+      await task
+    except asyncio.CancelledError:
+      pass
 
 if __name__ == "__main__":
   # Setup logging, and parse any arguments and pass them to main
